ed253b57d4055478e23f60228edf963b887e353a
Dmitry Bogomolov <dmitry.bogomolov.mail@gmail.com>
2022-05-17 00:07:40 +0300

    Add more stubs

--- bin.py
+++ bin.py
@@ -1,7 +1,9 @@
 #!/usr/bin/env python3
 
 import argparse
+from function_create import create_function
 from function_update import update_function
+from function_invoke import invoke_function
 
 def main() -> None:
     parser = argparse.ArgumentParser(
@@ -9,11 +11,23 @@ def main() -> None:
     )
     subparsers = parser.add_subparsers(dest='command')
 
-    update_parser = subparsers.add_parser(
-        name='function-update',
-        description='Update function'
+    create_function_parser = subparsers.add_parser(
+        name='function-create',
+        description='Create function',
     )
-    update_parser.add_argument('target_dir', help='path to directory')
+    create_function_parser.add_argument('target_dir', help='path to directory')
+
+    update_function_parser = subparsers.add_parser(
+        name='function-update',
+        description='Update function',
+    )
+    update_function_parser.add_argument('target_dir', help='path to directory')
+
+    invoke_function_parser = subparsers.add_parser(
+        name='invoke-function',
+        description='Invoke function',
+    )
+    invoke_function_parser.add_argument('target_dir', help='path to directory')
 
     args = parser.parse_args()
     command = args.command
@@ -21,8 +35,12 @@ def main() -> None:
         parser.print_help()
         return
 
+    if command == 'function-create':
+        create_function(args.target_dir)
     if command == 'function-update':
         update_function(args.target_dir)
+    if command == 'function-invoke':
+        invoke_function(args.target_dir)
 
 if __name__ == '__main__':
     main()
---
+++ function_create.py
@@ -0,0 +1,4 @@
+from os import path
+
+def create_function(dir_path: str) -> None:
+    dir_path = path.abspath(dir_path)
