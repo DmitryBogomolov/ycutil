c399c881adb86bed149af0ffa1fd420a20db69ec
Dmitry Bogomolov <dmitry.bogomolov.mail@gmail.com>
2022-05-17 10:50:22 +0300

    Put draft create

--- function_create.py
+++ function_create.py
@@ -1,4 +1,13 @@
-from config import read_config
+from config import read_config, Config
+from subprocess import run
 
 def create_function(dir_path: str) -> None:
     cfg = read_config(dir_path)
+    call_yc(cfg)
+
+def call_yc(cfg: Config) -> None:
+    args = [
+        'yc', 'serverless', 'function', 'create',
+        '--name', cfg.name,
+    ]
+    run(args, check=True)
