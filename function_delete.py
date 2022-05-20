4cd937d8eed1123deec3638dfacfce0029eb5567
Dmitry Bogomolov <dmitry.bogomolov.mail@gmail.com>
2022-05-20 17:50:34 +0300

    Typo

--- function_delete.py
+++ function_delete.py
@@ -4,7 +4,7 @@ from config import Config
 from logger import logger
 from yc_runner import run_yc
 
-def delete_function(dir_path) -> FunctionInfo:
+def delete_function(dir_path: str) -> FunctionInfo:
     logger.info('# delete function #')
     cfg = Config.from_dir(dir_path)
     out, _ = run_yc(['delete', '--name', cfg.name, '--no-user-output', '--format', 'json'])
