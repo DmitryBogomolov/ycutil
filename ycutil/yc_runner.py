59fed222e983910c6ab0a8ac49574d39f2b9e8c1
Dmitry Bogomolov <dmitry.bogomolov.mail@gmail.com>
2022-05-22 20:47:54 +0300

    Update

--- ycutil/yc_runner.py
+++ ycutil/yc_runner.py
@@ -4,18 +4,18 @@ from .logger import logger
 
 def run_yc(*args: List[str]) -> Tuple[str, str]:
     run_args = ['yc', 'serverless', 'function', *args, '--no-user-output', '--format', 'json']
-    logger.info('yc call: %s', ' '.join(run_args[2:]))
+    logger.info('yc:call: %s', ' '.join(run_args[2:]))
     try:
         proc = run(run_args, check=True, encoding='utf8', stdout=PIPE, stderr=PIPE)
         if proc.stdout:
-            logger.info('yc out: %s', proc.stdout)
+            logger.info('yc:out: %s', proc.stdout)
         if proc.stderr:
-            logger.info('yc err: %s', proc.stderr)
+            logger.info('yc:err: %s', proc.stderr)
         return (proc.stdout, proc.stderr)
     except CalledProcessError as err:
         if err.stdout:
-            logger.error('yc out: %s', err.stdout)
+            logger.error('yc:out: %s', err.stdout)
         if err.stderr:
-            logger.error('yc err: %s', err.stderr)
+            logger.error('yc:err: %s', err.stderr)
         message = err.stderr + err.stdout
         raise RuntimeError(message) from err
