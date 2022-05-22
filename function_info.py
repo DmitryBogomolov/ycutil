d4edd0fab4bda62a136cd50462ad0c1263812a61
Dmitry Bogomolov <dmitry.bogomolov.mail@gmail.com>
2022-05-22 10:30:04 +0300

    Add more fields

--- function_info.py
+++ function_info.py
@@ -8,6 +8,7 @@ class FunctionInfo(NamedTuple):
     created_at: datetime
     status: str
     invoke_url: str
+    log_group_id: str
 
     @classmethod
     def from_json(cls, content: Dict[str, Any]) -> 'FunctionInfo':
@@ -17,6 +18,7 @@ class FunctionInfo(NamedTuple):
             created_at = parse_date(content['created_at']),
             status = content['status'],
             invoke_url = content['http_invoke_url'],
+            log_group_id = content['log_group_id'],
         )
 
 class FunctionVersionInfo(NamedTuple):
@@ -25,6 +27,10 @@ class FunctionVersionInfo(NamedTuple):
     created_at: datetime
     log_group_id: str
     status: str
+    entrypoint: str
+    runtime: str
+    memory: int
+    timeout: int
 
     @classmethod
     def from_json(cls, content: Dict[str, Any]) -> 'FunctionVersionInfo':
@@ -34,4 +40,8 @@ class FunctionVersionInfo(NamedTuple):
             created_at = parse_date(content['created_at']),
             log_group_id = content['log_group_id'],
             status = content['status'],
+            entrypoint = content['entrypoint'],
+            runtime = content['runtime'],
+            memory = int(content['resources']['memory']) >> 20,
+            timeout = int(content['execution_timeout'][:-1]),
         )
