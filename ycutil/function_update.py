a3a9480876d57505ce1e46ce6545a7a6d994634f
Dmitry Bogomolov <dmitry.bogomolov.mail@gmail.com>
2022-05-23 15:48:45 +0300

    Remove helper

--- ycutil/function_update.py
+++ ycutil/function_update.py
@@ -14,8 +14,16 @@ def update_function(dir_path: str) -> FunctionVersionInfo:
     with TemporaryDirectory(dir=cfg.root_dir) as tmp_path:
         zip_path = path.join(tmp_path, cfg.name + '.zip')
         pack_code(zip_path, cfg.root_dir)
-        output = call_yc(cfg, zip_path)
-    return FunctionVersionInfo.from_json(load_json(output))
+        out, _ = run_yc(
+            'version', 'create',
+            '--function-name', cfg.name,
+            '--entrypoint', cfg.entrypoint,
+            '--runtime', cfg.runtime,
+            '--memory', f'{cfg.memory}m',
+            '--execution-timeout', f'{cfg.timeout}s',
+            '--source-path', zip_path,
+        )
+    return FunctionVersionInfo.from_json(load_json(out))
 
 def pack_code(zip_path: str, dir_path: str) -> None:
     with ZipFile(zip_path, mode='w') as zip_file:
@@ -29,18 +37,6 @@ def walk_code(root_path: str, zip_file: ZipFile, dir_path: str) -> None:
             zip_file.write(item_path, path.relpath(item_path, root_path))
         elif path.isdir(item_path):
             walk_code(root_path, zip_file, item_path)
-
-def call_yc(cfg: Config, zip_path: str) -> str:
-    out, _ = run_yc(
-        'version', 'create',
-        '--function-name', cfg.name,
-        '--entrypoint', cfg.entrypoint,
-        '--runtime', cfg.runtime,
-        '--memory', f'{cfg.memory}m',
-        '--execution-timeout', f'{cfg.timeout}s',
-        '--source-path', zip_path,
-    )
-    return out
 
 def get_function_versions(dir_path: str) -> List[FunctionVersionInfo]:
     logger.info('# get function versions #')
