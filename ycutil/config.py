21050bcbb240d7fa219db6da8a191d970f03440b
Dmitry Bogomolov <dmitry.bogomolov.mail@gmail.com>
2022-05-29 17:59:28 +0300

    Rework Config class

--- ycutil/config.py
+++ ycutil/config.py
@@ -1,7 +1,5 @@
-from typing import NamedTuple, Dict, Any, cast
 from os import path
 from json import load as load_json
-from .logger import logger
 
 CONFIG_NAME = '.ycconf'
 RUNTIMES = set(['python37', 'python38', 'python39'])
@@ -9,44 +7,45 @@ DEFAULT_TIMEOUT = 10
 DEFAULT_MEMORY = 128
 DEFAULT_RUNTIME = 'python37'
 
-class Config(NamedTuple):
+class Config:
     root_dir: str
     name: str
     entrypoint: str
-    timeout: int = DEFAULT_TIMEOUT
-    memory: int = DEFAULT_MEMORY
-    runtime: str = DEFAULT_RUNTIME
+    timeout: int
+    memory: int
+    runtime: str
+
+    def __init__(
+        self,
+        dir_path: str,
+        name: str,
+        entrypoint: str,
+        timeout: int = DEFAULT_TIMEOUT,
+        memory: int = DEFAULT_MEMORY,
+        runtime: str = DEFAULT_RUNTIME,
+    ):
+        if not dir_path:
+            raise ValueError(f'bad "dir_path": {dir_path}')
+        if not name:
+            raise ValueError(f'bad "name": {name}')
+        if not entrypoint:
+            raise ValueError(f'bad "entrypoint": {entrypoint}')
+        if timeout < 1:
+            raise ValueError(f'bad "timeout": {timeout}')
+        if memory < 128 or memory % 128 != 0:
+            raise ValueError(f'bad "memory": {memory}')
+        if not runtime in RUNTIMES:
+            raise ValueError(f'bad "runtime": {runtime}')
+        self.root_dir = path.abspath(dir_path)
+        self.name = name
+        self.entrypoint = entrypoint
+        self.timeout = timeout
+        self.memory = memory
+        self.runtime = runtime
 
     @classmethod
     def from_dir(cls, dir_path: str) -> 'Config':
-        root_dir = path.abspath(dir_path)
-        file_path = path.join(root_dir, CONFIG_NAME)
+        file_path = path.join(dir_path, CONFIG_NAME)
         with open(file_path, encoding='utf8') as file_obj:
-            content = cast(Dict[str, Any], load_json(file_obj))
-        name = content.pop('name', '')
-        if not name:
-            raise ValueError('no "name" field')
-        entrypoint = content.pop('entrypoint', '')
-        if not entrypoint:
-            raise ValueError('no "entrypoint" field')
-        timeout = content.pop('timeout', DEFAULT_TIMEOUT)
-        if not isinstance(timeout, int) or timeout < 1:
-            raise ValueError(f'bad "timeout" field: {timeout}')
-        memory = content.pop('memory', DEFAULT_MEMORY)
-        if not isinstance(memory, int) or memory % 128 != 0 or memory < 128:
-            raise ValueError(f'bad "memory" field: {memory}')
-        runtime = content.pop('runtime', DEFAULT_RUNTIME)
-        if runtime not in RUNTIMES:
-            raise ValueError(f'bad "runtime" field: {runtime}')
-        if len(content) > 0:
-            fields = ', '.join(content.keys())
-            raise ValueError(f'extra fields: {fields}')
-        logger.info('directory: %s', root_dir)
-        return cls(
-            root_dir=root_dir,
-            name=name,
-            entrypoint=entrypoint,
-            timeout=timeout,
-            memory=memory,
-            runtime=runtime,
-        )
+            content = load_json(file_obj)
+        return cls(dir_path=dir_path, **content)
