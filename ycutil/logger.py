bac4efe2b99da0b5668143cf70bb0cc0c105ea94
Dmitry Bogomolov <dmitry.bogomolov.mail@gmail.com>
2022-05-31 23:27:45 +0300

    Update logger

--- ycutil/logger.py
+++ ycutil/logger.py
@@ -1,9 +1,13 @@
-from sys import stdout
+from io import StringIO
 from logging import getLogger, Formatter, StreamHandler, INFO
 
 logger = getLogger('ycutil')
-handler = StreamHandler(stdout)
+str_buffer = StringIO()
+handler = StreamHandler(str_buffer)
 formatter = Formatter('%(asctime)s %(name)s %(levelname)-8s %(message)s')
 handler.setFormatter(formatter)
 logger.setLevel(INFO)
 logger.addHandler(handler)
+
+def get_log() -> str:
+    return str_buffer.getvalue()
