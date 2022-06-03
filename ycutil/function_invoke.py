a7e6f1515b990c89a2e024f5c2636e1dd0e030dd
Dmitry Bogomolov <dmitry.bogomolov.mail@gmail.com>
2022-06-03 12:31:48 +0300

    Fix parameter pass

--- ycutil/function_invoke.py
+++ ycutil/function_invoke.py
@@ -1,14 +1,23 @@
 from typing import Any
 from requests import post
+from json import dumps as dump_json
 from .config import Config
 from .yc_runner import run_yc
+
+def pack_data(data: Any) -> str:
+    if isinstance(data, str):
+        return data
+    if isinstance(data, (int, float)):
+        return str(data)
+    if isinstance(data, bool):
+        return 'true' if data else 'false'
+    return dump_json(data)
 
 def invoke_function(cfg: Config, data: Any = None) -> Any:
     '''Invoke function'''
     args = ['invoke', '--name', cfg.name]
     if data is not None:
-        args.append('--data')
-        args.append(str(data))
+        args = [*args, '--data', pack_data(data)]
     return run_yc(*args)
 
 def invoke_function_url(cfg: Config, data: Any = None) -> Any:
