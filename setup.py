8781a2e588995e21df128580359a5fea0fdc2d57
Dmitry Bogomolov <dmitry.bogomolov.mail@gmail.com>
2022-06-07 14:13:28 +0300

    Add setup.py

---
+++ setup.py
@@ -0,0 +1,19 @@
+from platform import python_version
+from setuptools import setup
+
+with open('./README.md', encoding='utf8') as file_obj:
+    long_description = file_obj.read()
+
+with open('./requirements.txt', encoding='utf8') as file_obj:
+    requirements = file_obj.readlines()
+
+setup(
+    name='ycfunc',
+    version='0.0.1',
+    license='MIT',
+    description='Yandex Cloud functions wrapper',
+    long_description=long_description,
+    packages=['ycfunc'],
+    install_requires=requirements,
+    python_requires='>=3.6',
+)
